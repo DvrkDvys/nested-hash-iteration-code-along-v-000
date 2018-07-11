92c86ea0b51c37cdbe69eb00eac798d1946579d8
Jordan Harris <jharris@wework.com>
2018-07-11 14:40:26 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -21,7 +21,7 @@ def remove_strawberry(contacts)
   
       if attribute == :favorite_ice_cream_flavors
         value.each do |flavor|
-          flavor.delete_if {|flavor| flavor = "strawberry"} 
+          flavor.delete {|flavor| flavor = "strawberry"} 
           
         # flavor.delete if flavor == "strawberry"
         # # binding.pry
